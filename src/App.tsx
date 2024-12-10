dd395a5fe7ef459a1b8138670df00c8b494833e9
sig-ume <beyondthemoonface@gmail.com>
2024-12-10 18:52:22 +0900

    背景色を青っぽい灰色に

--- src/App.tsx
+++ src/App.tsx
@@ -4,25 +4,29 @@ import TwitterStatusChecker from './components/TwitterStatusChecker';
 import TwitterStatusResults from './components/TwitterStatusCheckerResults';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import TwitterStatusHistory from './components/TwitterStatusHistory';
+
 function App() {
   return (
-    <Router>
-      <div className="container mx-auto py-8">
-        <h1 className="text-4xl font-bold text-center mb-8">
-          Xポスト検索除外チェッカー
-        </h1>
-        <Routes>
-          <Route path="/" element={<TwitterStatusChecker />} />
-          <Route path="/results" element={<TwitterStatusResults />} />
-          <Route path="/history/:sessionId" element={<TwitterStatusHistory />} />
-        </Routes>
-        <div className="mt-4 text-center text-sm text-slate-500">
-          <p>Xの投稿URLを入力することで、その投稿が検索除外されているかどうかを確認できます。</p>
-        </div>
-
-        <TopPageAdsense3 />
+    <div className="fixed inset-0 bg-slate-100"> {/* 画面全体をカバーする背景レイヤー */}
+      <div className="relative"> {/* コンテンツを配置するための相対位置の親要素 */}
+        <Router>
+          <div className="max-w-2xl mx-auto py-8 bg-slate-100">
+            <h1 className="text-4xl font-bold text-center mb-8">
+              Xポスト検索除外チェッカー
+            </h1>
+            <Routes>
+              <Route path="/" element={<TwitterStatusChecker />} />
+              <Route path="/results" element={<TwitterStatusResults />} />
+              <Route path="/history/:sessionId" element={<TwitterStatusHistory />} />
+            </Routes>
+            <div className="mt-4 text-center text-sm text-slate-500">
+              <p>Xの投稿URLを入力することで、その投稿が検索除外されているかどうかを確認できます。</p>
+            </div>
+            <TopPageAdsense3 />
+          </div>
+        </Router>
       </div>
-    </Router>
+    </div>
   );
 }
 
