30176cb380e9db16f93bfd926891f4298f819e93
sig-ume <beyondthemoonface@gmail.com>
2025-01-05 22:29:27 +0900

    チェックボックスの文面再々修正

--- src/components/ShadowbanChecker.tsx
+++ src/components/ShadowbanChecker.tsx
@@ -179,7 +179,7 @@ const ShadowbanChecker = () => {
                                 className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500"
                             />
                             <label htmlFor="searchban-check" className="text-sm text-gray-600">
-                                直近20件のポストの検索除外をチェックする
+                                直近20件のポストが検索除外されているかチェックする
                             </label>
                         </div>
                         <div className="flex items-center space-x-2">
