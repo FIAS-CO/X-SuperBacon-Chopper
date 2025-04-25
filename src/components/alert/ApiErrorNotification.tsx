419c9b92466052f982a194caddd478e45964ba58
sig-ume <beyondthemoonface@gmail.com>
2025-04-26 01:36:06 +0900

    封印第三段

--- src/components/alert/ApiErrorNotification.tsx
+++ src/components/alert/ApiErrorNotification.tsx
@@ -10,7 +10,7 @@ export const ApiErrorNotification: React.FC = () => {
     return (
         <Alert variant="destructive" className="mb-6 max-w-screen-xl mx-auto">
             <AlertCircle className="h-5 w-5" />
-            <AlertTitle className="ml-2 font-semibold">お知らせ(2025/04/01 9:49)</AlertTitle>
+            <AlertTitle className="ml-2 font-semibold">お知らせ(2025/04/26)</AlertTitle>
             <AlertDescription className="ml-7 text-sm">
                 Xの仕様変更に伴いシャドウバンチェッカーが正常に動作しなくなっております。<br />
                 ※Xポスト検索除外チェッカーは正常に機能しております。<br />
