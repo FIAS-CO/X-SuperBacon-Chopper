2e2a078da3d469aaed38e4b551026ace464754ab
sig-ume <beyondthemoonface@gmail.com>
2025-04-30 05:02:44 +0900

    一部の封印を解除

--- src/components/alert/ApiErrorNotification.tsx
+++ src/components/alert/ApiErrorNotification.tsx
@@ -10,15 +10,17 @@ export const ApiErrorNotification: React.FC = () => {
     return (
         <Alert variant="destructive" className="mb-6 max-w-screen-xl mx-auto">
             <AlertCircle className="h-5 w-5" />
-            <AlertTitle className="ml-2 font-semibold">お知らせ(2025/04/26)</AlertTitle>
+            <AlertTitle className="ml-2 font-semibold">お知らせ(2025/04/30)</AlertTitle>
             <AlertDescription className="ml-7 text-sm">
-                Xの仕様変更に伴いシャドウバンチェッカーが正常に動作しなくなっております。<br />
-                ※Xポスト検索除外チェッカーは正常に機能しております。<br />
+                Xの仕様変更に対応し、シャドウバンチェックを復活いたしました。<br />
+                ただ、申し訳ありませんが「直近20件のポストが検索除外されているかチェックする」機能は引き続き復旧対応中です。<br />
                 復旧するまでご迷惑をおかけしますが、しばらくお待ち下さい。<br />
+                引き続き"X（Twitter）Shadowban Checker F"をよろしくお願いいたします。<br />
                 <br />
-                Due to X's specification changes, the shadow ban checker is not functioning properly.<br />
-                * The X post search exclusion checker is functioning normally.<br />
-                We apologize for the inconvenience until it is restored. Please wait for a while.
+                We have updated the system to accommodate X's specification changes, and the shadowban check feature has been restored.<br />
+                However, we apologize that the function to "check whether the latest 20 posts are excluded from search results" is still under restoration.<br />
+                We apologize for the inconvenience and appreciate your patience until it is fully restored.<br />
+                Thank you for your continued support of the "X (Twitter) Shadowban Checker F".<br />
             </AlertDescription>
         </Alert>
     );
