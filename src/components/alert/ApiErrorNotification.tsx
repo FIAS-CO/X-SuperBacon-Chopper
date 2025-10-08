83fd5525cb2470af485cd09002e4460d4cb8a5ac
sig-ume <beyondthemoonface@gmail.com>
2025-10-08 13:56:07 +0900

    文面ちょっと直した

--- src/components/alert/ApiErrorNotification.tsx
+++ src/components/alert/ApiErrorNotification.tsx
@@ -13,13 +13,13 @@ export const ApiErrorNotification: React.FC = () => {
             <AlertTitle className="ml-2 font-semibold">お知らせ(2025/05/08)</AlertTitle>
             <AlertDescription className="ml-7 text-sm">
                 いつもご利用いただきありがとうございます。<br />
-                諸事情により、現在シャドウバンチェッカーのメンテナンスを実施中のためチェックがエラーとなります。<br />
+                現在シャドウバンチェッカーのメンテナンス中のためチェックがエラーとなります。<br />
                 再開までしばらくお待ち下さい。<br />
                 <br />
                 ご迷惑をおかけして申し訳ありません。<br />
                 <br />
                 Thank you for using our service.<br />
-                Due to various circumstances, the Shadowban Checker is currently under maintenance and checks will result in errors.<br />
+                Shadowban Checker is currently under maintenance and checks will result in errors.<br />
                 Please wait for a while until it resumes.<br />
                 <br />
                 We apologize for any inconvenience caused.
