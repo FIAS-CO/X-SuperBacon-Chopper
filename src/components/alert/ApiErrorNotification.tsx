80cffc82f826a583a538c37403a655940ca3933f
sig-ume <beyondthemoonface@gmail.com>
2025-11-11 10:33:44 +0900

    告知削除

--- src/components/alert/ApiErrorNotification.tsx
+++ src/components/alert/ApiErrorNotification.tsx
@@ -10,10 +10,10 @@ export const ApiErrorNotification: React.FC = () => {
     return (
         <Alert variant="destructive" className="mb-6 max-w-screen-xl mx-auto">
             <AlertCircle className="h-5 w-5" />
-            <AlertTitle className="ml-2 font-semibold">お知らせ(2025/11/09)</AlertTitle>
+            <AlertTitle className="ml-2 font-semibold">お知らせ(2025/11/11)</AlertTitle>
             <AlertDescription className="ml-7 text-sm">
                 いつもご利用いただきありがとうございます。<br />
-                現在、直近のポストの検索除外チェックが使用できなくなっております。<br />
+                直近のポストの検索除外チェックが使用できなくなっております。<br />
                 復旧までしばらくお待ち下さい。<br />
                 <br />
                 Thank you for using our service.<br />
